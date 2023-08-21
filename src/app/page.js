524db39cbdf4b57574a72e74369e01b0a58f234f
reyhane <reyhanehalipour78@gmail.com>
2023-08-21 23:21:30 +0430

    import components

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,31 @@
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+import Hero from "./Components/Hero"
+import HoursSelection from "./Components/HoursSelection"
+import Serach from "./Components/Serach"
+import Why from "./Components/Why"
+import Testimonial from "./Components/Testimonial"
+import TestimonialSlider from "./Components/TestimonialSlider"
+import SerachMobail from "./Components/SerachMobail"
+import Cars from "./Components/Cars"
+import About from "./Components/About"
+import Cta from "./Components/Cta"
+import BackToTopBtn from "./Components/BackToTopBtn"
+
 
 
 export default function Home() {
   return (
-    <div>main</div>
+    <>
+      <Header/>
+      <Hero/>
+      <Cars/>
+      <About/>
+      <Why/>
+      <Testimonial/>
+      <Cta/>
+      <Footer/>
+      <BackToTopBtn/>
+    </>
   )
 }
