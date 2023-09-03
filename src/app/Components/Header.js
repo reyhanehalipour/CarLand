fb469e15674552c02245f1ff83004740b81dec7e
reyhane <reyhanehalipour78@gmail.com>
2023-09-03 15:15:56 +0430

    create header

--- src/app/Components/Header.js
+++ src/app/Components/Header.js
@@ -49,9 +49,9 @@ useEffect(()=>{
      max-w-[1920px] mx-auto z-20 transition-all duration-300`}>
 
     <div className="xl:container mx-auto flex flex-col xl:flex-row 
-     xl:items-center xl:justify-between"> </div>
+     xl:items-center xl:justify-between"> 
 
-    <div className="flex justify-between items-center px-4">
+    <div className="flex justity-between items-center px-4">
       <Link 
       to="home"
       smooth={DesktopMode}
@@ -62,11 +62,95 @@ useEffect(()=>{
 
       </Link>
       
-      <div className="cursor-pointer xl:hidden">
-      {Nav ? (<BiX className="text-4xl"/>): (<BiMenuAltRight className="text-4xl"/>)}
+      <div className="cursor-pointer xl:hidden" onClick={()=>SetNav(!Nav)}>
+
+      {Nav ? (<BiX className="text-4xl"/>):
+       (<BiMenuAltRight className="text-4xl"/>)}
+
       </div>
-      
+
+    
     </div>
+
+   
+    <nav className={`${ Nav? 'max-h-max py-8 px-4 xl:px-0 xl:py-0 flex flex-col':'max-h-0 xl:max-h-max '}
+        flex flex-col w-full bg-white gap-y-6 overflow-hidden font-bold
+          xl:font-medium xl:flex-row  xl:w-max xl:gap-x-8 xl:h-max xl:bg-transparent 
+          transition-all duration-150 text-center xl:text-left uppercase text-sm
+           xl:text-[15px] xl:normal-case`}>
+        <Link 
+        className='cursor-pointer'
+               to="home"
+               activeClass="active"
+              smooth={DesktopMode}
+              spy={true}
+             >Home
+      </Link>
+
+      <Link 
+        className='cursor-pointer'
+               to="cars"
+               activeClass="active"
+              smooth={DesktopMode}
+              spy={true}
+             >Cars
+      </Link>
+
+
+      <Link 
+        className='cursor-pointer'
+               to="about"
+               activeClass="active"
+              smooth={DesktopMode}
+              spy={true}
+             >About
+      </Link>
+
+      <Link 
+        className='cursor-pointer'
+               to="why"
+               activeClass="active"
+              smooth={DesktopMode}
+              spy={true}
+             >Why
+      </Link>
+
+
+      <Link 
+        className='cursor-pointer'
+               to="home"
+               activeClass="active"
+              smooth={DesktopMode}
+              spy={true}
+             >use Testimonial
+      </Link>
+
+      <Link 
+        className='cursor-pointer'
+               to="cantact"
+               activeClass="active"
+              smooth={DesktopMode}
+              spy={true}
+             >Contact
+      </Link>
+
+
+      <Link 
+        className='xl:hidden btn btn-primary btn-sm max-w-[164px] mx-auto py-6'
+               to="/"
+               activeClass="active"
+              smooth={DesktopMode}
+              spy={true}
+             >see all cars
+      </Link>
+
+      <SerachMobail/>
+        </nav>
+   
+
+    <div>
+    </div>
+       </div>
    </header>
   )
 }
